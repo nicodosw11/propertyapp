f24d7d221bbf61b37e23dc88223fc852a5e6246e
Nicolas Dotto <nicolas.dotto@hotmail.com>
2017-02-10 16:21:11 +0000

    smtp still not working in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
