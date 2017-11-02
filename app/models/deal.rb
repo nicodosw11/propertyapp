2c8c57779264407d235a271cdd22bdbf8f4322fe
Nicolas Dotto <nicolas.dotto@hotmail.com>
2017-11-02 07:06:37 +0000

    prepare design for deals and deal

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -7,4 +7,13 @@ class Deal < ApplicationRecord
   has_many :tenants, dependent: :destroy
   has_many :investments, dependent: :nullify
   has_many :users, through: :investments
+
+  def set_part_value
+    if self.nb_investors > 0
+      self.valuation.to_f / self.nb_investors.to_f
+    else
+      self.valuation
+    end
+  end
+
 end
