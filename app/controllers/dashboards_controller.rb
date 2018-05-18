40307641adc7e913e28f17e1f5371cb93a97c9ac
Nicolas Dotto <nicolas.dotto@hotmail.com>
2018-05-18 08:27:27 +0100

    sortable columns with direction

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,21 @@
 class DashboardsController < ApplicationController
   before_action :authenticate_user!
   skip_after_action :verify_authorized, :verify_policy_scoped
+  helper_method :sort_column, :sort_direction
 
   def index
     @investments = current_user.investments
-    @transactions = current_user.investments.order(params[:sort])
+    @transactions = current_user.investments.order(sort_column + " " + sort_direction)
+  end
+
+  private
+
+  def sort_column
+    Investment.column_names.include?(params[:sort]) ? params[:sort] : "deal_id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,4 +27,10 @@ module ApplicationHelper
     end
     link_to(name, '#', class: "add_fields " + args[:class], data: {id: id, fields: fields.gsub("\n", "")})
   end
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
 end
